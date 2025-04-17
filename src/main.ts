dd21de481f0d01a094ec533a489bde41e51a5e99
NikeBoy128 <haroldmosquera739@gmail.com>
2025-04-17 18:49:01 -0500

    :sparkles: add express-basic-auth for API authentication and configure basic auth for Swagger UI

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,33 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import * as basicAuth from 'express-basic-auth';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = new ConfigService();
 
+  const swaggerUser = configService.get<string>('SWAGGER_USER');
+  const swaggerPassword = configService.get<string>('SWAGGER_PASS');
+  app.use(
+    ['/api'],
+    basicAuth({
+      challenge: true,
+      users: {
+        [swaggerUser]: swaggerPassword,
+      },
+    }),
+  );
   const config = new DocumentBuilder()
     .setTitle('Simplest API')
     .setVersion('1.0')
-
     .addBearerAuth()
     .build();
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -21,7 +36,13 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  app.enableCors({
+    origin: '*',
+  });
+
   app.setGlobalPrefix('api/v1');
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
