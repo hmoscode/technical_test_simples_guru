8a43acb137c1dd56f6c7dd2ff8f955084ee07b31
NikeBoy128 <haroldmosquera739@gmail.com>
2025-04-17 20:27:37 -0500

    :bug: fix AuthTokenResponseDto definition and add ApiProperty decorators for access and refresh tokens

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { AuthUseCase } from '../useCase/auth.UseCase';
 import { AuthTokenResponseDto, SignInDto } from '../dtos/auth.dto';
 import { seconds, Throttle } from '@nestjs/throttler';
@@ -10,6 +10,9 @@ export class AuthController {
   constructor(private readonly authUseCase: AuthUseCase) {}
 
   @Post('sign-in')
+  @ApiOkResponse({
+    type: AuthTokenResponseDto,
+  })
   @Throttle({
     short: {
       limit: 5,
--- src/auth/dtos/auth.dto.ts
+++ src/auth/dtos/auth.dto.ts
@@ -1,7 +1,19 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString } from 'class-validator';
-export interface AuthTokenResponseDto {
+
+export class AuthTokenResponseDto {
+  @ApiProperty({
+    type: String,
+    description: 'Access token for authentication',
+    example: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...',
+  })
   accessToken: string;
+
+  @ApiProperty({
+    type: String,
+    description: 'Refresh token for obtaining a new access token',
+    example: 'dGhpcyBpcyBhIHJlZnJlc2ggdG9rZW4uLi4=',
+  })
   refreshToken: string;
 }
 export interface AuthTokenPayloadModel {
